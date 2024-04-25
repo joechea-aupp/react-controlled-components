c04e98fa70e84735046b7d15dc8c623e43d3ce87
joechea <s.chea@aupp.edu.kh>
2024-04-25 16:45:48 +0700

    initial push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MessageBox from "./MessageBox";
+
+const App = () => (
+  <MessageBox
+    nameVal="John Doe"
+    natureVal="Tech Support"
+    queryVal="I've been having problems with my account for a while. Please help me resolve it asap!"
+  />
+);
+
+export default App;
